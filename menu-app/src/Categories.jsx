462c3b392bc7d1c43b2c63b59ee59df485c73a49
Henrique Antonelo <henrique.antonelo@gmail.com>
2024-07-10 02:36:43 -0300

    Creating NavBar and Categories buttons list

---
+++ menu-app/src/Categories.jsx
@@ -0,0 +1,22 @@
+import "./Categories.css";
+
+function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li>
+          <a href="#" className="categories-item"></a>
+          <a href="#" className="categories-item"></a>
+          <a href="#" className="categories-item"></a>
+          <a href="#" className="categories-item"></a>
+          <a href="#" className="categories-item"></a>
+          <a href="#" className="categories-item"></a>
+          <a href="#" className="categories-item"></a>
+          <a href="#" className="categories-item"></a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
